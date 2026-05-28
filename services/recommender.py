aa8a32b93bb0d10d649d8d1e0a213472b3631a3d
Erik67-u <waadenth@katharineum.de>
2026-05-28 10:57:12 +0000

    recommender eingefügt

--- services/recommender.py
+++ services/recommender.py
@@ -1,1 +1,24 @@
+import json
 
+
+def recommend_exercises(
+        detected_equipment
+):
+
+    with open(
+        "database/exercises.json",
+        "r"
+    ) as file:
+
+        database = json.load(file)
+
+    exercises = []
+
+    for equipment in detected_equipment:
+
+        if equipment in database:
+            exercises.extend(
+                database[equipment]
+            )
+
+    return exercises
