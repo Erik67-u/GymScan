20bc0733a4c2efe8d8b7d736e6b69a950e9d2ef9
Erik67-u <waadenth@katharineum.de>
2026-05-28 10:54:56 +0000

    detector py eingefügt

--- services/detector.py
+++ services/detector.py
@@ -1,1 +1,15 @@
+from models.clip_model import classify_equipment
+from models.owlvit_model import detect_objects
 
+
+def detect_equipment(image):
+
+    owl_results = detect_objects(image)
+
+    clip_results = classify_equipment(image)
+
+    final_equipment = list(
+        set(owl_results + clip_results)
+    )
+
+    return final_equipment
